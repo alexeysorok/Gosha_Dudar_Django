c951b949f5e7ae8ac17195fb1145fed69f802f7f
alexe <alexe@W10SSD>
2019-09-13 01:31:07 +0300

    Передача данных  в HTML

--- django-examples/mysite/mainApp/views.py
+++ django-examples/mysite/mainApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'mainApp/home.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/contact.html', 
+    {'values': ['Если у вас остались вопросы, то задавайте их мне по телефону', '(123-123-123)',
+    'test@mail.ru']})
