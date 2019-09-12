a82a1dc86d0737bb3eeb03abffd646a5c1999bfe
alexe <alexe@W10SSD>
2019-09-13 00:22:14 +0300

    create new app mainApp

--- django-examples/mysite/mysite/urls.py
+++ django-examples/mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webexample/', include('webexample.urls'))
+    # path('webexample/', include('webexample.urls')),
+    path('', include('mainApp.urls')),
 ]
