ab9357402b2e4d25b5a62b1e0765559b2f0608e1
alexe <alexe@W10SSD>
2019-09-13 02:18:45 +0300

    fix and migrations

--- django-examples/mysite/news/urls.py
+++ django-examples/mysite/news/urls.py
@@ -5,7 +5,7 @@ from news.models import *
 urlpatterns = [
     
     path('', 
-        ListView.as_view(queryset=Articles.objects.all().orderby('-date')[:20]), 
-        template_name='news/posts.html' ),
+        ListView.as_view(queryset=Articles.objects.all().order_by('-date')[:20], 
+        template_name='news/posts.html' )),
     
 ]
